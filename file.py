6570be1543352b160189e6c3fcb7d8e24545c518
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-18 09:26:12 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,14 @@
 print("Welcome to the School Portal Login System!")
 email = input("enter your email: ")
+
 password = input("emter your password: ")
 
 
 def signup(email,password):
     if "@" not in email:
-        print("Error. Invalid email.")
+        print("Error. Invalid email.(╯°□°）╯︵ ┻━┻ bad you👎")
         return "invalid email"
     print("yay u!👍 good email")
+
+    
 signup(email,password)
