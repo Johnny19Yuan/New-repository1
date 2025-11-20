2a2ef30d53816d83d75805115a1c2976173e9443
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-20 09:09:19 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 print("Welcome to the School Portal Login System!")
 email = input("enter your email: ")
 
-
+users = []
 
 def signup(email):
     if "@" not in email:
@@ -10,16 +10,28 @@ def signup(email):
     print("yay u!👍 good email")
 
     password = input("Enter your password: ")
-    upper = False
+    upper = 0
+    num = 0
+
     if password.__len__() < 8:
         print("Error. Password Needs to be at least 8 characters long.")
         return "invalid password"
     for char in password:
-        if char == char.upper():
-            upper = True
-        
-            
-
-
+        if char.isupper():
+            upper =+ 1
+    for char in password:
+        if char.isdigit():
+            num =+ 1
+    if upper <1 :
+        print("Password needs to have at least 1 uppercase letter")
+        return "invalid password"
+    if num <1:
+        print("Password needs to have at least 1 number")
+        return "invalid password"
+    print("yay u Good password :D")
     
+    
+
+
+
 signup(email)
