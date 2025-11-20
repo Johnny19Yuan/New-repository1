95a6ade787acb58680c9229d6071105fec728d82
Johnny19Yuan <johnny19yuan@gmail.com>
2025-11-20 09:25:50 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,18 +1,14 @@
 print("Welcome to the School Portal Login System!")
 email = input("enter your email: ")
-
-users = []
-
+users = [{"email": ,"password":}]
 def signup(email):
     if "@" not in email:
         print("Error. Email must contain @ symbol.")
         return "invalid email"
     print("yay u!👍 good email")
-
     password = input("Enter your password: ")
     upper = 0
     num = 0
-
     if password.__len__() < 8:
         print("Error. Password Needs to be at least 8 characters long.")
         return "invalid password"
@@ -28,10 +24,7 @@ def signup(email):
     if num <1:
         print("Password needs to have at least 1 number")
         return "invalid password"
-    print("yay u Good password :D")
-    
-    
-
-
-
+    print("yay u! Good password :D")
+    users.append(email)
 signup(email)
+print(users)
